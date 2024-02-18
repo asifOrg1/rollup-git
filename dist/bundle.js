694e3e7a7d3d4a37045e04ed11679196c15ce214
ShadmaanAsif <shadmaan.asif@seera.sa>
2024-02-19 00:01:13 +0400

    fix: chore

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+	// function logger() {
+	//   console.log('sss');
+	// }
+
+	// export default logger;
+	console.log('ss');
+
+}));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// function logger() {
+//   console.log('sss');
+// }
+
+// export default logger;
+console.log('ss');
